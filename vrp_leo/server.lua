4c650e63cf863596475d132709b7eeb72d9d5471
Gabew19 <47000024+Gabew19@users.noreply.github.com>
2019-02-21 06:57:44 -0500

    Getting Somewhere

--- vrp_leo/server.lua
+++ vrp_leo/server.lua
@@ -8,16 +8,17 @@ local lang = Lang.new(module("vrp_leo", "cfg/lang/"..cfg.lang) or {})
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP","vRP_Leo")
 
-AddEventHandler('chatMessage', function(source, name, msg)
-	if msg == "/leo" then
+RegisterServerEvent("leo")
+AddEventHandler("leo", function()
 	  local user_id = vRP.getUserId({source})
 	  local player = vRP.getUserSource({user_id})
 	  if vRP.hasGroup({user_id,"cop"}) then
+	  CancelEvent();
         TriggerEvent("chatMessage", "[Dispatch]", { 255, 0, 0 }, "10-4 Showing you 10-8 available.")
-    end
-      if vRP.hasGroup({user_id,"nil"}) then
+	  if vRP.hasGroup({user_id,"unemployment"}) then
         TriggerEvent("chatMessage", "[Dispatch]", { 255, 0, 0 }, "You Are not a cop!")
 		end
 	end
 end)
+
 
