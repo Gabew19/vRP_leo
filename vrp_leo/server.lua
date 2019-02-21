12e7f44a081e31b8f38124f593fadfd98d1b6204
Gabew19 <47000024+Gabew19@users.noreply.github.com>
2019-02-21 06:08:08 -0500

    what am i doing with my life lol
    Updated server.lua corrected some errors

--- vrp_leo/server.lua
+++ vrp_leo/server.lua
@@ -8,19 +8,16 @@ local lang = Lang.new(module("vrp_leo", "cfg/lang/"..cfg.lang) or {})
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP","vRP_Leo")
 
---/leo to go onduty
-LocalCop = false
-
 AddEventHandler('chatMessage', function(source, name, msg)
 	if msg == "/leo" then
 	  local user_id = vRP.getUserId({source})
 	  local player = vRP.getUserSource({user_id})
 	  if vRP.hasGroup({user_id,"cop"}) then
-        TriggerEvent("chatMessage"), "[Dispatch]", { 255, 0, 0 }, "10-4 Showing you 10-8 available."
+        TriggerEvent("chatMessage", "[Dispatch]", { 255, 0, 0 }, "10-4 Showing you 10-8 available.")
     end
-      if vRP.hasGroup({user_id,""}) then
-        TriggerEvent("chatMessage"), "[Dispatch]", { 255, 0, 0 }, "You Are not a cop!"
-    end
+      if vRP.hasGroup({user_id,"nil"}) then
+        TriggerEvent("chatMessage", "[Dispatch]", { 255, 0, 0 }, "You Are not a cop!")
+		end
+	end
 end)
-
 
