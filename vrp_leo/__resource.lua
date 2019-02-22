392ebdcf5ce17337c4a2657bcc8d0d943695cb2a
Gabew19 <47000024+Gabew19@users.noreply.github.com>
2019-02-22 00:55:21 -0500

    Updated again

--- vrp_leo/__resource.lua
+++ vrp_leo/__resource.lua
@@ -5,6 +5,12 @@ description "vRP Leo"
 
 dependency "vrp"
 
-client_script "client.lua"
+client_script{
+  "client.lua"
+  'lib/Proxy.lua',
+  }
 
-server_script "server.lua"
+server_script{
+  "server.lua"
+  "@vrp/lib/utils.lua",
+}
