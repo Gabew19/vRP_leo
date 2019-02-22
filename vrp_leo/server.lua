2887592fc037b942b25e41658ba0c61448a755ff
Gabew19 <47000024+Gabew19@users.noreply.github.com>
2019-02-22 06:06:35 -0500

    actually works thanks to vrp support team!
    All it does is say ur on duty. dosent give guns or anything of that nature.

--- vrp_leo/server.lua
+++ vrp_leo/server.lua
@@ -1,24 +1,14 @@
 local Tunnel = module("vrp", "lib/Tunnel")
 local Proxy = module("vrp", "lib/Proxy")
 
-   -- Loads Global Lang and local Lang
-local glang = Lang.new(module("vrp", "cfg/lang/"..cfg.lang) or {})
-local lang = Lang.new(module("vrp_leo", "cfg/lang/"..cfg.lang) or {})
-
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vRP","vRP_Leo")
 
-RegisterServerEvent("leo")
-AddEventHandler("leo", function()
-	  local user_id = vRP.getUserId({source})
-	  local player = vRP.getUserSource({user_id})
-	  if vRP.hasGroup({user_id,"cop"}) then
-	  CancelEvent();
-        TriggerEvent("chatMessage", "[Dispatch]", { 255, 0, 0 }, "10-4 Showing you 10-8 available.")
-	  if vRP.hasGroup({user_id,"unemployment"}) then
-        TriggerEvent("chatMessage", "[Dispatch]", { 255, 0, 0 }, "You Are not a cop!")
-		end
-	end
+RegisterCommand('leo', function(source)
+    local user_id = vRP.getUserId({source})
+      if vRP.hasGroup({user_id,"cop"}) then
+        TriggerClientEvent("chatMessage", source, "[Dispatch]", { 255, 0, 0 }, "10-4 Showing you 10-8 available.")
+    else
+        TriggerClientEvent("chatMessage", source, "[Dispatch]", { 255, 0, 0 }, "You Are not a cop!")
+        end
 end)
-
-
