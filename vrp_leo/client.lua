e6a1990586fb29e57d548c5db9397322860b35e7
Gabew19 <47000024+Gabew19@users.noreply.github.com>
2019-02-22 06:07:51 -0500

    got rid of useless code

--- vrp_leo/client.lua
+++ vrp_leo/client.lua
@@ -2,7 +2,3 @@ vRPmt = {}
 local Tunnel = module("vrp", "lib/Tunnel") 
 vRP = Proxy.getInterface("vRP")
 vRPclient = Tunnel.getInterface("vrp_Leo",vRPmt)
-
-RegisterCommand("leo",function(source, args)
-    TriggerServerEvent("leo", source)
-end, false)
